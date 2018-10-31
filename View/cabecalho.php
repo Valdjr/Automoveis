32155a5cafb6ea41ab44cab115c70fc7ec2c73f8
Valdir Júnior <31621712+Valdjr@users.noreply.github.com>
2018-10-31 11:24:20 -0300

    Atualização do fontawesome para cdn

--- View/cabecalho.php
+++ View/cabecalho.php
@@ -10,6 +10,6 @@
     <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="css/custom.css">
-    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
     <title>Automoveis</title>
 
